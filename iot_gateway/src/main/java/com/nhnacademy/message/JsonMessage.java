68fcae571d67843de97d262b848f3878931bfb97
shkcreate0518 <shkcreate0518@gmail.com>
2023-11-28 14:01:12 +0900

    JsonObject

--- iot_gateway/src/main/java/com/nhnacademy/message/JsonMessage.java
+++ iot_gateway/src/main/java/com/nhnacademy/message/JsonMessage.java
@@ -1,5 +1,16 @@
 package com.nhnacademy.message;
 
+import org.json.simple.JSONObject;
+
 public class JsonMessage extends Message {
+    JSONObject jsonObject;
+
+    public JSONObject getJsonObject() {
+        return jsonObject;
+    }
+
+    public void setJsonObject(JSONObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
 
 }
