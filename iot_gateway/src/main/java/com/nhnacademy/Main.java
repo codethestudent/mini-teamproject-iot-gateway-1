92b42b9bc9b9851bcfd2d2177a6c51be0480952d
doramonz <32805709+doramonz@users.noreply.github.com>
2023-11-28 11:10:31 +0900

    test

--- iot_gateway/src/main/java/com/nhnacademy/Main.java
+++ iot_gateway/src/main/java/com/nhnacademy/Main.java
@@ -1,7 +1,52 @@
 package com.nhnacademy;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws MqttException, InterruptedException {
+        String publisherId = UUID.randomUUID().toString();
+        IMqttClient publisher = new MqttClient("tcp://ems.nhnacademy.com", publisherId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        options.setWill("application/will", "Disconnected".getBytes(), 1, false);
+        options.setKeepAliveInterval(10000);
+        options.setExecutorServiceTimeout(0);
+        publisher.connect(options);
+
+        MqttMessage message = new MqttMessage("Hello".getBytes());
+        publisher.publish("data", message);
+        String application_name = "application";
+        publisher.subscribe(application_name + "/#", (topic, msg) -> {
+            String[] topics = topic.split("/");
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new String(msg.getPayload()));
+            JSONObject jsonObj = (JSONObject) obj;
+
+            if (jsonObj.containsKey("object"))
+                if (((JSONObject) jsonObj.get("deviceInfo")).get("tenantName").equals("NHN Academy 경남")) {
+                    System.out.println("deviceinfo : " + jsonObj.get("deviceInfo"));
+                    System.out.println("object : " + jsonObj.get("object"));
+                    HashMap<String,String> value = (HashMap) jsonObj.get("object");
+                    if(value.containsKey("temperature"))
+                    System.out.println(value.get("temperature"));
+                }
+        });
+        while (!Thread.currentThread().isInterrupted()) {
+            Thread.sleep(100);
+        }
+        publisher.disconnect();
+        publisher.close();
     }
 }
