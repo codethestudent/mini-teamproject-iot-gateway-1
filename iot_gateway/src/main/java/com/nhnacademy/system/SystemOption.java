b96d7a7160cadc05ca5168f2e90db002487d8094
{qaw302} <{osshun21@gmail.com}>
2023-11-28 17:13:20 +0900

    fix : option으로 수정

--- iot_gateway/src/main/java/com/nhnacademy/system/SystemOption.java
+++ iot_gateway/src/main/java/com/nhnacademy/system/SystemOption.java
@@ -1,21 +1,45 @@
 package com.nhnacademy.system;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class SystemOption {
     private static SystemOption systemOption;
 
-    private static String DEFAULT = "--an \"application\" -s temperature,humidity";
-    private String commandLine;
+    private static String[] DEFAULT = { "--an", "\"application\"", "-s", "temperature,humidity" };
+    private String[] args;
     private String applicationName;
     private String[] sensors;
+    private Options options;
 
-    private SystemOption(String commandLine) {
-        this.commandLine = commandLine;
+    private SystemOption(String[] args) {
+        options = new Options();
+        this.args = args;
+        options.addOption("-an", true, "application name");
+        options.addOption("s", true, "sensor");
         setInfo();
     }
 
     private void setInfo() {
-        applicationName = commandLine.split(" ")[1];
-        sensors = commandLine.split(" ")[3].split(",");
+        // applicationName = commandLine.split(" ")[1];
+        // sensors = commandLine.split(" ")[3].split(",");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine;
+        try {
+            commandLine = parser.parse(options, this.args);
+            if (commandLine.hasOption("-an")) {
+                applicationName = commandLine.getOptionValue("-an");
+            }
+            if (commandLine.hasOption("s")) {
+                sensors = commandLine.getOptionValue("s").split(",");
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     public static SystemOption getSystemOption() {
@@ -26,9 +50,9 @@ public class SystemOption {
         return systemOption;
     }
 
-    public SystemOption getSystemOption(String commandLine) {
+    public static SystemOption getSystemOption(String[] args) {
         if (systemOption == null) {
-            systemOption = new SystemOption(commandLine);
+            systemOption = new SystemOption(args);
         }
 
         return systemOption;
