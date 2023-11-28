72e38ebb6495e8ed22fcd4db6df70ff6bb883cca
codeTheStudent <hjun303@gnu.ac.kr>
2023-11-28 16:05:13 +0900

    Feat: mqtt 연결 성공

--- iot_gateway/src/main/java/com/nhnacademy/node/MqttInNode.java
+++ iot_gateway/src/main/java/com/nhnacademy/node/MqttInNode.java
@@ -1,23 +1,58 @@
 package com.nhnacademy.node;
 
-public class MqttInNode extends InputNode {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
-    MqttInNode(int count) {
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.json.simple.JSONObject;
+
+import com.nhnacademy.message.JsonMessage;
+
+public class MqttInNode extends InputNode {
+    private List<String> sensorList;
+    private String publisherId;
+    private JSONObject object;
+
+    public MqttInNode(int count) {
         super(count);
     }
 
-    MqttInNode() {
+    public MqttInNode() {
         this(1);
     }
 
     @Override
     void preprocess() {
+        sensorList = new ArrayList<>();
+        publisherId = UUID.randomUUID().toString();
+        object = new JSONObject();
 
     }
 
     @Override
     void process() {
 
+        try (IMqttClient client = new MqttClient("tcp://localhost:1883", publisherId)) {
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
+            options.setConnectionTimeout(10);
+
+            client.connect(options);
+            client.subscribe("#", (topic, msg) -> {
+                System.out.println("Message received : " + topic + msg);
+                object.put("topic", topic);
+                object.put("payload", msg);
+                JsonMessage messageObject = new JsonMessage(object);
+                output(messageObject);
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
