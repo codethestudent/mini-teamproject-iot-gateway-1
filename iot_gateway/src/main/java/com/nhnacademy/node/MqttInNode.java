d166c00726075430ee47c49dce4eb9a0013122bc
codeTheStudent <hjun303@gnu.ac.kr>
2023-11-28 13:56:45 +0900

    wire 수정

---
+++ iot_gateway/src/main/java/com/nhnacademy/node/MqttInNode.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.node;
+
+public class MqttInNode extends InputNode {
+
+    MqttInNode(int count) {
+        super(count);
+    }
+
+    MqttInNode() {
+        this(1);
+    }
+
+    @Override
+    void preprocess() {
+
+    }
+
+    @Override
+    void process() {
+
+    }
+
+    @Override
+    void postprocess() {
+
+    }
+}
