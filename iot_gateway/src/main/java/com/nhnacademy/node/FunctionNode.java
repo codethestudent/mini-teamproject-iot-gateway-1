d5c7690535a07994f5c978f50a31e61fcf394b10
doramonz <32805709+doramonz@users.noreply.github.com>
2023-11-28 13:58:34 +0900

    .

---
+++ iot_gateway/src/main/java/com/nhnacademy/node/FunctionNode.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.node;
+
+public class FunctionNode extends InputOutputNode {
+
+    FunctionNode(int inCount, int outCount) {
+        super(inCount, outCount);
+        //TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    void process(){
+        
+    }
+}
