2993067459a935f24e87413c64ec6c45423d12f5
halo-eunsu <ujd017@naver.com>
2023-11-30 13:46:05 +0900

    log 파일 최종

--- src/main/java/com/nhnacademy/node/LogsInputOutputNode.java
+++ src/main/java/com/nhnacademy/node/LogsInputOutputNode.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.json.simple.JSONObject;
 import com.nhnacademy.message.JsonMessage;
@@ -38,6 +39,9 @@ public class LogsInputOutputNode extends InputOutputNode {
 
                     if (message instanceof JsonMessage) {
                         JsonMessage jsonMessage = (JsonMessage) message;
+                        //parse한 객체 생성
+
+                        JSONObject jsonlog = createJsonLog(jsonMessage);
 
                         // 파일이 없으면 생성
                         File file = new File(path);
@@ -49,11 +53,17 @@ public class LogsInputOutputNode extends InputOutputNode {
                             }
                         }
 
+                        
+
+
                         // 기존 데이터 읽어오기
                         List<String> existingData = readExistingData(file);
 
                         // 새로운 데이터 추가
-                        existingData.add(jsonMessage.toString());
+
+                        //existingData.add(jsonMessage.getJsonObject().toJSONString());
+                        existingData.add(jsonlog.toJSONString());
+                       
 
                         // JSON 파일 쓰기
                         writeDataToFile(file, existingData);
@@ -88,4 +98,40 @@ public class LogsInputOutputNode extends InputOutputNode {
             System.out.println("Write success!");
         }
     }
+
+    private JSONObject createJsonLog(JsonMessage jsonMessage) {
+        JSONObject jsonLog = new JSONObject();
+
+        // "time" 값 추출
+        long timeValue = extractTimeValue(jsonMessage);
+        jsonLog.put("time", timeValue);
+
+        // "/p/class_a" 값 추출
+        String topic = jsonMessage.getJsonObject().get("topic").toString();
+        String[] topicParts = topic.split("/");
+        String placeValue = "";
+        for (String part : topicParts) {
+            if (part.equals("p")) {
+                placeValue = topicParts[Arrays.asList(topicParts).indexOf(part) + 1];
+                break;
+            }
+        }
+        jsonLog.put("place", placeValue);
+
+        return jsonLog;
+    }
+
+    private long extractTimeValue(JsonMessage jsonMessage) {
+        Object payloadObj = jsonMessage.getJsonObject().get("payload");
+        if (payloadObj instanceof JSONObject) {
+            JSONObject payloadJson = (JSONObject) payloadObj;
+            Object timeObj = payloadJson.get("time");
+            if (timeObj instanceof Long) {
+                return (Long) timeObj;
+            }
+        }
+        return 0; // 기본 값 또는 오류 처리에 맞게 반환
+    }
+
+    
 }
