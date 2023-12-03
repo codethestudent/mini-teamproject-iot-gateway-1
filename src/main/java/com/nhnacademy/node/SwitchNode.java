4e55df339935b31662aad19e8c9ef734b4adacdc
codeTheStudent <hjun303@gnu.ac.kr>
2023-12-03 15:26:54 +0900

    feat: switch node

--- src/main/java/com/nhnacademy/node/SwitchNode.java
+++ src/main/java/com/nhnacademy/node/SwitchNode.java
@@ -1,13 +1,18 @@
 package com.nhnacademy.node;
 
+/*
+ * 일단 여기 들어오는 데이터는 데이터를 확인한 후 해당 키 가 있
+ */
+
 public class SwitchNode extends InputOutputNode {
+
 
     SwitchNode(int inCount, int outCount) {
         super(inCount, outCount);
     }
 
     void preprocess() {
-        
+
     }   
 
     void process() {
