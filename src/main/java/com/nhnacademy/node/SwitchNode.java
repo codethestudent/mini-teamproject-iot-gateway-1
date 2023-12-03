b1c2735cada3c444ad33d552351c29447313f03a
codeTheStudent <hjun303@gnu.ac.kr>
2023-12-03 15:26:19 +0900

    feat : switch node

---
+++ src/main/java/com/nhnacademy/node/SwitchNode.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.node;
+
+public class SwitchNode extends InputOutputNode {
+
+    SwitchNode(int inCount, int outCount) {
+        super(inCount, outCount);
+    }
+
+    void preprocess() {
+        
+    }   
+
+    void process() {
+
+    }
+
+    void postprocess() {
+
+    }
+}
