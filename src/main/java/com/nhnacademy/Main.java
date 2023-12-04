8e4e8efaf835d3186afaf12729d26a4ce3307b23
doramonz <32805709+doramonz@users.noreply.github.com>
2023-12-04 09:41:05 +0900

    .

--- src/main/java/com/nhnacademy/Main.java
+++ src/main/java/com/nhnacademy/Main.java
@@ -4,7 +4,8 @@ import com.nhnacademy.system.NodeRedSystem;
 
 public class Main {
     public static void main(String[] args) {
-        NodeRedSystem.getInstance().generateDefaultFlows("ems.nhnacademy.com",
-                new String[] { "temperature", "humidity", "co2" });
+        // NodeRedSystem.getInstance().generateDefaultFlows("ems.nhnacademy.com",
+        //         new String[] { "temperature", "humidity", "co2" });
+        NodeRedSystem.getInstance().load("flows.json");
     }
 }
