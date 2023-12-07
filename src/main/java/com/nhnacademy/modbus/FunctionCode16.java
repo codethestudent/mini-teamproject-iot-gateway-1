b2057845377e61943554a29b494e9e219a6dfa64
codeTheStudent <hjun303@gnu.ac.kr>
2023-12-07 18:11:16 +0900

    feat: functionCode 16

--- src/main/java/com/nhnacademy/modbus/FunctionCode16.java
+++ src/main/java/com/nhnacademy/modbus/FunctionCode16.java
@@ -1,30 +1,52 @@
 package com.nhnacademy.modbus;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import com.nhnacademy.exception.InvalidArgumentException;
 
 public class FunctionCode16 {
     // Write Multiple Registers 16
 
-    public void writeMultipleRegisters(byte[] pdu) {
+    public byte[] makeWriteMultipleRegistersRequest(int address, int quantity, byte[] pdu) {
         // fc, address, address, quantity, quantity, byte count, Register values(Nbytes)
         // address : 10101 (0010 0111 0111 0101)-> {16, 39, 117, x, x, x, x ...}
 
-        byte[] tempDB = new byte[40000];
+        byte[] frame = new byte[6 + pdu.length];
 
-        int address = (pdu[1] << 8) | (pdu[2] & 0xFF);
-        int quantity = (pdu[3] << 8) | (pdu[4] & 0xFF);
-        int registersByte = (pdu[5] & 0xFF);
-        byte[] registersValues = new byte[pdu.length - 6];
+        ByteBuffer b = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
 
-        System.arraycopy(pdu, 5, registersValues, 0, pdu.length - 5);
+        frame[0] = 0x10;
 
-        if (registersByte != registersValues.length || registersByte / 2 != quantity) {
-            throw new InvalidArgumentException();
+        b.putInt(address);
+        frame[1] = b.get(2);
+        frame[2] = b.get(3);
+
+        b.clear();
+        b.putInt(quantity);
+        frame[3] = b.get(2);
+        frame[4] = b.get(3);
+
+        frame[5] = (byte) (quantity < 127 || quantity > 0 ? quantity * 2 : quantity);
+
+        if (quantity * 2 != pdu.length) {
+            throw new IllegalArgumentException();
         }
-        int j = 0;
-        for (int i = address; i < quantity; i++) {
-            tempDB[i] = registersValues[j++];
-        }
 
+        System.arraycopy(pdu, 0, frame, 6, pdu.length);
+
+        return frame;
+    }
+
+    public static void main(String[] args) {
+        FunctionCode16 code = new FunctionCode16();
+        byte[] b = new byte[] {1,2,3,4,5,6};
+        
+
+        byte[] frame = code.makeWriteMultipleRegistersRequest(10101, 3, b);
+
+        for(byte a : frame){
+            System.out.println(a);
+        }
     }
 }
